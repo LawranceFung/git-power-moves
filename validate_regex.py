c465fb738da9a946576ee16e402789cecfebf404
Lawrance Fung <fungjl@wustl.edu>
2023-04-26 14:54:03 -0500

    add comment to assure sanctity of input

--- validate_regex.py
+++ validate_regex.py
@@ -1,7 +1,7 @@
 # Toy script for demonstrating regex
 import re, sys, getopt
 
-argv = sys.argv[1:]
+argv = sys.argv[1:] # for quality assurance wrap each arg in ''
 # Bellefontaine
 valid = []
 for arg in argv:
@@ -11,6 +11,7 @@ for arg in argv:
         except re.error:
             print(arg + " is non valid regex pattern")
         else:
+            print(arg)
             valid.append(arg)
 
 print("Valid: " + ", ".join(valid))
