ca26644a2620575c45b7a11af3da766c646fa38f
Lawrance Fung <fungjl@wustl.edu>
2023-04-26 14:51:03 -0500

    enforce uniqueness to output

--- validate_regex.py
+++ validate_regex.py
@@ -5,11 +5,12 @@ argv = sys.argv[1:]
 # Bellefontaine
 valid = []
 for arg in argv:
-    try:
-        re.compile(arg)
-    except re.error:
-        print(arg + " is non valid regex pattern")
-    else:
-        valid.append(arg)
+    if arg not in valid:
+        try:
+            re.compile(arg)
+        except re.error:
+            print(arg + " is non valid regex pattern")
+        else:
+            valid.append(arg)
 
 print("Valid: " + ", ".join(valid))
